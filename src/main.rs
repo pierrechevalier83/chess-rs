f8c0ceea84852fe4ef9ef594b7061ba373c7e20a
Pierre Chevalier <pierrechevalier83@gmail.com>
2018-03-06 20:53:20 +0000

    Remember selection after cell is clicked

--- src/main.rs
+++ src/main.rs
@@ -20,25 +20,36 @@ where
     ).unwrap();
 }
 
+struct Selection {
+    pos: (usize, usize),
+    cell: cell::Cell<char>,
+}
+
 fn get_cell(display: &mut MatrixDisplay<char>, x: u16, y: u16) -> cell::Cell<char> {
     display
         .cell_at_cursor_position((x as usize, y as usize))
         .clone()
 }
 
-fn set_bg(display: &mut MatrixDisplay<char>, x: u16, y: u16, bg: u8) {
-    display
-        .cell_at_cursor_position((x as usize, y as usize))
-        .color
-        .bg = bg;
+fn set_bg(display: &mut MatrixDisplay<char>, pos: (usize, usize), bg: u8) {
+    display.cell_at_cursor_position((pos.0, pos.1)).color.bg = bg;
 }
 
 fn redraw(
     mut stdout: &mut MouseTerminal<termion::raw::RawTerminal<std::io::Stdout>>,
-    display: &MatrixDisplay<char>,
+    mut display: &mut MatrixDisplay<char>,
+    selection: &Option<Selection>,
 ) {
+    match selection {
+        &Some(ref sel) => set_bg(&mut display, sel.pos, 23),
+        &None => (),
+    }
     clear(&mut stdout);
     display.print(&mut stdout, &style::BordersStyle::None);
+    match selection {
+        &Some(ref sel) => set_bg(&mut display, sel.pos, sel.cell.color.bg),
+        &None => (),
+    }
 }
 
 fn initial_grid() -> (Format, matrix::Matrix<cell::Cell<char>>) {
@@ -51,17 +62,19 @@ fn main() {
     let (format, mut data) = initial_grid();
     let mut display = MatrixDisplay::new(&format, &mut data);
     let mut stdout = MouseTerminal::from(std::io::stdout().into_raw_mode().unwrap());
-    redraw(&mut stdout, &display);
+    let mut selection: Option<Selection> = None;
+    redraw(&mut stdout, &mut display, &selection);
     for input in std::io::stdin().events() {
         let evt = input.unwrap();
         match evt {
             Event::Key(Key::Char('q')) => break,
             Event::Mouse(me) => match me {
                 MouseEvent::Press(_, x, y) => {
-                    let previous_cell = get_cell(&mut display, x, y);
-                    set_bg(&mut display, x, y, 23);
-                    redraw(&mut stdout, &display);
-                    set_bg(&mut display, x, y, previous_cell.color.bg);
+                    selection = Some(Selection {
+                        pos: (x as usize, y as usize),
+                        cell: get_cell(&mut display, x, y),
+                    });
+                    redraw(&mut stdout, &mut display, &selection);
                 }
                 _ => (),
             },
