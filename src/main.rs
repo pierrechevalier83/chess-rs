b08eb566bf3188e54a9d37255ddfb54614548317
Pierre Chevalier <pierrechevalier83@gmail.com>
2018-03-08 22:35:25 +0000

    Use chess::Board as main data representation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate chess;
 extern crate matrix_display;
 extern crate termion;
 
@@ -5,7 +6,6 @@ use matrix_display::*;
 use termion::input::{MouseTerminal, TermRead};
 use termion::event::{Event, Key, MouseEvent};
 use termion::raw::IntoRawMode;
-mod xchess;
 
 fn clear<W>(out: &mut W)
 where
@@ -67,14 +67,94 @@ fn redraw(
     }
 }
 
-fn initial_grid() -> (Format, matrix::Matrix<cell::Cell<char>>) {
+fn square_bg_in_ansi(square: chess::Square) -> u8 {
+    let ansi_white = 0;
+    let ansi_dark = 7;
+    if (square.get_rank().to_index() + square.get_file().to_index()) % 2 == 0 {
+        ansi_white
+    } else {
+        ansi_dark
+    }
+}
+
+fn to_unicode(piece: chess::Piece, color: chess::Color) -> char {
+    use chess::Piece::*;
+    match color {
+        chess::Color::White => match piece {
+            Pawn => '♙',
+            Knight => '♘',
+            Bishop => '♗',
+            Rook => '♖',
+            Queen => '♕',
+            King => '♔',
+        },
+        chess::Color::Black => match piece {
+            Pawn => '♟',
+            Knight => '♞',
+            Bishop => '♝',
+            Rook => '♜',
+            Queen => '♛',
+            King => '♚',
+        },
+    }
+}
+
+fn piece_on_square(board: chess::Board, square: chess::Square, color: chess::Color) -> char {
+    match board.piece_on(square) {
+        Some(piece) => to_unicode(piece, color),
+        None => ' ',
+    }
+}
+
+fn color_of_piece(board: chess::Board, square: chess::Square) -> chess::Color {
+    let bb = chess::BitBoard::from_square(square);
+    if (board.color_combined(chess::Color::White) & bb) == bb {
+        chess::Color::White
+    } else {
+        chess::Color::Black
+    }
+}
+
+fn rank_to_display(board: chess::Board, rank: &chess::Rank) -> Vec<cell::Cell<char>> {
+    let ansi_blue = 33;
+    chess::ALL_FILES
+        .iter()
+        .map(|file| {
+            let square = chess::Square::make_square(*rank, *file);
+
+            cell::Cell::new(
+                piece_on_square(board, square, color_of_piece(board, square)),
+                ansi_blue,
+                square_bg_in_ansi(square),
+            )
+        })
+        .collect()
+}
+
+fn board_to_display(board: chess::Board) -> matrix::Matrix<cell::Cell<char>> {
+    let all_squares = chess::ALL_RANKS
+        .iter()
+        .rev()
+        .flat_map(|rank| rank_to_display(board, rank))
+        .collect();
+    matrix::Matrix::new(chess::NUM_RANKS, all_squares)
+}
+
+fn initial_grid() -> (
+    Format,
+    matrix::Matrix<cell::Cell<char>>,
+    Option<chess::Board>,
+) {
     let format = Format::new(7, 3);
-    let board = xchess::read_xchess("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-    (format, matrix::Matrix::new(8, board))
+    let starting_position = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    let chess_board = chess::Board::from_fen(starting_position.to_string());
+    (format, board_to_display(chess_board.unwrap()), chess_board)
 }
 
 fn main() {
-    let (format, mut data) = initial_grid();
+    let (format, mut data, board) = initial_grid();
+    let text = format!("{}", board.unwrap());
+    println!("{}", text);
     let mut display = MatrixDisplay::new(&format, &mut data);
     let mut stdout = MouseTerminal::from(std::io::stdout().into_raw_mode().unwrap());
     let mut selection: Option<Selection> = None;
