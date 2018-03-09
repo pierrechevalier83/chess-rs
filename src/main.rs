de7ee21c4b38ac10d2b43f443e47499902768d8a
Pierre Chevalier <pierrechevalier83@gmail.com>
2018-03-09 01:48:09 +0000

    Only allow legal moves
    Use chess::Board to determine wether a move is legal.
    Never manually mess with the matrix_display. It is only a view.

--- src/main.rs
+++ src/main.rs
@@ -20,57 +20,13 @@ where
     ).unwrap();
 }
 
-#[derive(Clone)]
-struct Selection {
-    pos: (usize, usize),
-    cell: cell::Cell<char>,
-}
-
-fn get_cell(display: &mut MatrixDisplay<char>, pos: (usize, usize)) -> cell::Cell<char> {
-    display.cell_at_cursor_position(pos).clone()
-}
-
-fn set_bg(display: &mut MatrixDisplay<char>, pos: (usize, usize), bg: u8) {
-    display.cell_at_cursor_position(pos).color.bg = bg;
-}
-
-fn swap_cells(
-    display: &mut MatrixDisplay<char>,
-    left_pos: (usize, usize),
-    right_pos: (usize, usize),
-) {
-    let left_value = get_cell(display, left_pos).value;
-    display
-        .cell_at_cursor_position((left_pos.0, left_pos.1))
-        .value = display
-        .cell_at_cursor_position((right_pos.0, right_pos.1))
-        .value;
-    display
-        .cell_at_cursor_position((right_pos.0, right_pos.1))
-        .value = left_value;
-}
-
-fn redraw(
-    mut stdout: &mut MouseTerminal<termion::raw::RawTerminal<std::io::Stdout>>,
-    mut display: &mut MatrixDisplay<char>,
-    selection: &Option<Selection>,
-) {
-    match selection {
-        &Some(ref sel) => set_bg(&mut display, sel.pos, 23),
-        &None => (),
-    }
-    clear(&mut stdout);
-    display.print(&mut stdout, &style::BordersStyle::None);
-    match selection {
-        &Some(ref sel) => set_bg(&mut display, sel.pos, sel.cell.color.bg),
-        &None => (),
-    }
-}
-
-fn square_bg_in_ansi(square: chess::Square) -> u8 {
+fn square_bg_in_ansi(square: chess::Square, selection: &Option<chess::Square>) -> u8 {
     let ansi_white = 0;
     let ansi_dark = 7;
-    if (square.get_rank().to_index() + square.get_file().to_index()) % 2 == 0 {
+    let ansi_grey = 23;
+    if Some(square) == *selection {
+        ansi_grey
+    } else if (square.get_rank().to_index() + square.get_file().to_index()) % 2 == 0 {
         ansi_white
     } else {
         ansi_dark
@@ -115,7 +71,11 @@ fn color_of_piece(board: chess::Board, square: chess::Square) -> chess::Color {
     }
 }
 
-fn rank_to_display(board: chess::Board, rank: &chess::Rank) -> Vec<cell::Cell<char>> {
+fn rank_to_display(
+    board: chess::Board,
+    rank: &chess::Rank,
+    selection: &Option<chess::Square>,
+) -> Vec<cell::Cell<char>> {
     let ansi_blue = 33;
     chess::ALL_FILES
         .iter()
@@ -124,17 +84,20 @@ fn rank_to_display(board: chess::Board, rank: &chess::Rank) -> Vec<cell::Cell<ch
             cell::Cell::new(
                 piece_on_square(board, square, color_of_piece(board, square)),
                 ansi_blue,
-                square_bg_in_ansi(square),
+                square_bg_in_ansi(square, selection),
             )
         })
         .collect()
 }
 
-fn board_to_display(board: chess::Board) -> matrix::Matrix<cell::Cell<char>> {
+fn board_to_display(
+    board: chess::Board,
+    selection: &Option<chess::Square>,
+) -> matrix::Matrix<cell::Cell<char>> {
     let all_squares = chess::ALL_RANKS
         .iter()
         .rev()
-        .flat_map(|rank| rank_to_display(board, rank))
+        .flat_map(|rank| rank_to_display(board, rank, selection))
         .collect();
     matrix::Matrix::new(chess::NUM_RANKS, all_squares)
 }
@@ -144,46 +107,73 @@ fn initial_grid() -> chess::Board {
     chess::Board::from_fen(starting_position.to_string()).unwrap()
 }
 
-fn is_legal_move(board: chess::Board, origin: chess::Square, destination: chess::Square) -> bool {
-    // TODO: handle promotion (not always None)
-    board.legal(chess::ChessMove::new(origin, destination, None))
+fn is_legal_move(board: chess::Board, chess_move: chess::ChessMove) -> bool {
+    board.legal(chess_move)
 }
 
-fn click_to_square(x: u16, y: u16) -> chess::Square {
-    chess::Square::make_square(
-        chess::Rank::from_index(x as usize),
-        chess::File::from_index(y as usize),
-    )
+fn click_to_square(click: (u16, u16)) -> chess::Square {
+    let format = Format::new(7, 3);
+    let mut data = matrix::Matrix::new(
+        chess::NUM_RANKS,
+        [cell::Cell::new(' ', 0, 0)]
+            .iter()
+            .cycle()
+            .take(64)
+            .cloned()
+            .collect::<Vec<_>>(),
+    );
+    let display = MatrixDisplay::new(&format, &mut data);
+    println!("Clicked {}, {}", click.0, click.1);
+    let pos = display.coordinates_at_cursor_position((click.0 as usize, click.1 as usize));
+    println!("Pos {}, {}", pos.0, pos.1);
+    let sq = chess::Square::make_square(
+        chess::Rank::from_index(chess::NUM_RANKS - 1 - pos.1),
+        chess::File::from_index(pos.0),
+    );
+    println!("Square {}", sq);
+    sq
+}
+
+fn draw(
+    mut stdout: &mut MouseTerminal<termion::raw::RawTerminal<std::io::Stdout>>,
+    board: chess::Board,
+    selection: &Option<chess::Square>,
+) {
+    let format = Format::new(7, 3);
+    let mut data = board_to_display(board, selection);
+    let display = MatrixDisplay::new(&format, &mut data);
+    clear(&mut stdout);
+    display.print(&mut stdout, &style::BordersStyle::None);
 }
 
 fn main() {
-    let format = Format::new(7, 3);
-    let board = initial_grid();
-    let mut data = board_to_display(board);
-    let mut display = MatrixDisplay::new(&format, &mut data);
+    let mut board = initial_grid();
+    let mut selection: Option<chess::Square> = None;
     let mut stdout = MouseTerminal::from(std::io::stdout().into_raw_mode().unwrap());
-    let mut selection: Option<Selection> = None;
-    redraw(&mut stdout, &mut display, &selection);
+    draw(&mut stdout, board, &selection);
     for input in std::io::stdin().events() {
         let evt = input.unwrap();
         match evt {
             Event::Key(Key::Char('q')) => break,
             Event::Mouse(me) => match me {
                 MouseEvent::Press(_, x, y) => {
-                    let square = click_to_square(x, y);
-                    let pos = (square.get_rank().to_index(), square.get_file().to_index());
-                    let cell = get_cell(&mut display, pos);
+                    let square = click_to_square((x, y));
                     match selection {
-                        Some(sel) => if cell.value == ' ' && sel.cell.value != ' ' {
-                            swap_cells(&mut display, sel.pos, pos)
-                        },
-                        None => (),
+                        Some(previous_selection) => {
+                            let attempted_move =
+                                chess::ChessMove::new(previous_selection, square, None);
+                            if is_legal_move(board, attempted_move) {
+                                board = board.make_move(attempted_move);
+                            }
+                            selection = None;
+                        }
+                        None => {
+                            if board.piece_on(square) != None {
+                                selection = Some(square);
+                            }
+                        }
                     }
-                    selection = Some(Selection {
-                        pos: pos,
-                        cell: get_cell(&mut display, pos),
-                    });
-                    redraw(&mut stdout, &mut display, &selection);
+                    draw(&mut stdout, board, &selection);
                 }
                 _ => (),
             },
